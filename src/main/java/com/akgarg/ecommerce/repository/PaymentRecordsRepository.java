c0917c1250881042fd8e6a94c7d7baae783da2c6
Akhilesh Garg <72432094+akgarg0472@users.noreply.github.com>
2021-05-24 22:10:19 +0530

    Added method to fetch Payment record by order Id

--- src/main/java/com/akgarg/ecommerce/repository/PaymentRecordsRepository.java
+++ src/main/java/com/akgarg/ecommerce/repository/PaymentRecordsRepository.java
@@ -4,4 +4,6 @@ import com.akgarg.ecommerce.entity.PaymentRecords;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRecordsRepository extends JpaRepository<PaymentRecords, String> {
+
+    PaymentRecords getByOrderId(String orderId);
 }
